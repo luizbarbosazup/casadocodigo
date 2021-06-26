c1ca3d300048e1706e444bd998eecce6d92cb6c0
deveduardoalbuquerque <luiz.barbosa@zup.com.br>
2021-06-26 09:12:12 -0300

    initial setup

---
+++ src/test/java/br/com/zupedu/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
